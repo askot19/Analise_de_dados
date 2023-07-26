4c7dbaa532ea8f3010cd73db05d3dd9c18fda8b4
Paulo Henrique Ferreira <127543378+askot19@users.noreply.github.com>
2023-07-26 11:07:01 -0300

    Update Q09.sql

--- Steve's Car Showroom/Q09.sql
+++ Steve's Car Showroom/Q09.sql
@@ -1,4 +1,8 @@
 -- 9. What is the name and city of the salesperson who sold the most number of cars in the year 2023?
+-- 9. Qual o nome e a cidade do funcionário que vendeu o maior número de carros no ano de 2023?
+
+-- Esta questão permite enxergar o funcionário com o maior empenho em vendas, esta é uma nova métrica também associada ao desempenho, pois embora possa não ser o maior faturamento, o maior número de vendas pode estar associado a um funcionário que cativa os clientes.
+
 SELECT
      sp.name
    , sp.city  
