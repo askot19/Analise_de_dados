e0e4617f7341d6e28cdd8e624ee95d6b40f11e36
Paulo Henrique Ferreira <127543378+askot19@users.noreply.github.com>
2023-07-26 11:00:26 -0300

    Update Q06.sql

--- Steve's Car Showroom/Q06.sql
+++ Steve's Car Showroom/Q06.sql
@@ -1,4 +1,7 @@
 -- 6. What are the details of the cars sold in the year 2021 by salesperson 'Emily Wong'?
+-- 6. Quais são os detalhes dos carros vendidos pela 'Emily Wong', durante o ano de 2021?
+
+-- Esta questão permite observar as características específicas de um produto associadas a um funcionário ao longo de determinado ano. Elas permitem que identifiquemos as especialidades atreladas às vendas deste funcionário e estabelecer estratégias de desempenho para ele.
 SELECT
      sp.name AS salesperson
    , c.*
