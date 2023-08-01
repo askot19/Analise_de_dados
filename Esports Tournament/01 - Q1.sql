3f6c24bbe794ea569682d4197cea39d550dd71af
Paulo Henrique Ferreira <127543378+askot19@users.noreply.github.com>
2023-08-01 16:01:59 -0300

    Update 01 - Q1.sql

--- Esports Tournament/01 - Q1.sql
+++ Esports Tournament/01 - Q1.sql
@@ -1,4 +1,7 @@
 -- 1. What are the names of the players whose salary is greater than 100,000?
+-- 1. Quais são os nomes dos jogadores que possuem salário maior que 100.000?
+
+-- Esta questão pode nos servir para encontrar o jogadores mais custosos e questionar se estes altos salários estão associados aos jogadores com maiores desempenhos nos times.
 SELECT
      player_name
   ,  salary
